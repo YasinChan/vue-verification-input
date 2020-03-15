22166edff0af6f0d21f2babda35d60fba2e720b9
陈耀耀 <yasinchan2016@gmail.com>
2020-03-15 23:25:56 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,181 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <span>删除回调</span>
+      <span>{{ deleteNum }}</span>
+    </div>
+    <div>
+      <span>完成回调</span>
+      <span>{{ finishNum }}</span>
+    </div>
+    <h1>默认样式</h1>
+    <verificationCodeInput
+        :wrong="verifyCodeWrong"
+        @deleteEvent="deleteEvent"
+        @finishEvent="finishEvent"
+    />
+    <span>{{ remind }}</span>
+    <pre style="text-align: left;" v-highlightjs="sourcecode1"><code class=""></code></pre>
+
+    <h1>自定义样式</h1>
+    <verificationCodeInput
+        :wrong="verifyCodeWrong2"
+        item-class="item-class"
+        max-length="4"
+        item-margin="20px 10px"
+        item-active-class="item-active-class"
+        item-wrong-class="item-wrong-class"
+        @deleteEvent="deleteEvent2"
+        @finishEvent="finishEvent2"
+    />
+    <pre style="text-align: left;" v-highlightjs="sourcecode2"><code class=""></code></pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import 'highlight.js/styles/github.css'
+  import VerificationCodeInput from './components/VerificationCodeInput.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      VerificationCodeInput
+    },
+    data() {
+      return {
+        remind: '请输入验证码',
+        verifyCodeWrong: false,
+        verifyCodeWrong2: false,
+        deleteNum: 0,
+        finishNum: 0,
+        sourcecode1:
+            '<template>\n' +
+            '  <verificationCodeInput\n' +
+            '    :wrong="verifyCodeWrong"\n' +
+            '    @deleteEvent="deleteEvent"\n' +
+            '    @finishEvent="finishEvent"\n' +
+            '  />\n' +
+            '</template>\n' +
+            '// script \n' +
+            'export default {\n' +
+            '  methods: {\n' +
+            '    deleteEvent() {\n' +
+            '      this.deleteNum += 1\n' +
+            '      this.remind = \'请输入验证码\'\n' +
+            '      this.verifyCodeWrong = false\n' +
+            '    },\n' +
+            '    finishEvent() {\n' +
+            '      this.finishNum += 1\n' +
+            '      this.remind = \'验证码输入错误\'\n' +
+            '      this.verifyCodeWrong = true\n' +
+            '    },\n' +
+            '  }\n' +
+            '}\n',
+        sourcecode2:
+            '<template>\n' +
+            '  <verificationCodeInput\n' +
+            '    :wrong="verifyCodeWrong2"\n' +
+            '    item-class="item-class"\n' +
+            '    max-length="4"\n' +
+            '    item-margin="20px 10px"\n' +
+            '    item-active-class="item-active-class"\n' +
+            '    item-wrong-class="item-wrong-class"\n' +
+            '    @deleteEvent="deleteEvent2"\n' +
+            '    @finishEvent="finishEvent2"\n' +
+            '  />\n' +
+            '</template>\n' +
+            '\n' +
+            '// script \n' +
+            'export default {\n' +
+            '  methods: {\n' +
+            '    deleteEvent2() {\n' +
+            '      this.deleteNum += 1\n' +
+            '      this.verifyCodeWrong2 = false\n' +
+            '    },\n' +
+            '    finishEvent2() {\n' +
+            '      this.finishNum += 1\n' +
+            '      this.verifyCodeWrong2 = true\n' +
+            '    }\n' +
+            '  }\n' +
+            '}\n' +
+            '\n' +
+            '// css\n' +
+            '<style>\n' +
+            '  .item-class {\n' +
+            '    height: 80px;\n' +
+            '    width: 60px;\n' +
+            '    border: 1px solid #999;\n' +
+            '    background: #fff;\n' +
+            '    border-radius: 8px;\n' +
+            '    font-size: 26px;\n' +
+            '    color: #ccc;\n' +
+            '  }\n' +
+            '  .item-active-class {\n' +
+            '    border: 1px solid #1989fa;\n' +
+            '    color: #1989fa;\n' +
+            '    background: #f2f2f2;\n' +
+            '  }\n' +
+            '  .item-wrong-class {\n' +
+            '    border: 1px solid #e6a23c;\n' +
+            '    color: #fff;\n' +
+            '    background: #e6a23c;\n' +
+            '  }\n' +
+            '</style>'
+      }
+    },
+    methods: {
+      deleteEvent() {
+        this.deleteNum += 1
+        this.remind = '请输入验证码'
+        this.verifyCodeWrong = false
+      },
+      finishEvent() {
+        this.finishNum += 1
+        this.remind = '验证码输入错误'
+        this.verifyCodeWrong = true
+      },
+      deleteEvent2() {
+        this.deleteNum += 1
+        this.verifyCodeWrong2 = false
+      },
+      finishEvent2() {
+        this.finishNum += 1
+        this.verifyCodeWrong2 = true
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+  h1 {
+    margin: 20px 0 10px;
+  }
+  .item-class {
+    height: 80px;
+    width: 60px;
+    border: 1px solid #999;
+    background: #fff;
+    border-radius: 8px;
+    font-size: 26px;
+    color: #ccc;
+  }
+  .item-active-class {
+    border: 1px solid #1989fa;
+    color: #1989fa;
+    background: #f2f2f2;
+  }
+  .item-wrong-class {
+    border: 1px solid #e6a23c;
+    color: #fff;
+    background: #e6a23c;
+  }
 </style>
---
+++ src/components/VerificationCodeInput.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="verification-code">
+    <input
+        :maxlength="maxLength"
+        ref="codeInput"
+        v-model="code"
+        class="verification-code__input"
+        type="tel"
+        onselectstart="return false;"
+    />
+    <table class="verification-code__box" :style="{ borderSpacing: itemMargin }" @click="triggerInput">
+      <tr>
+        <td
+            v-for="(item, index) in itemList"
+            :key="index"
+            :class="['verification-code__item', itemClass, wrong ? itemWrongClass : '', item ? itemActiveClass : '']"
+        >
+          {{ item }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'VerificationCodeInput',
+  props: {
+    maxLength: {
+      default: 6
+    },
+    wrong: {
+      type: Boolean,
+      default: false
+    },
+    itemClass: {
+      type: String,
+      default: 'verification-code__item-class'
+    },
+    itemWrongClass: {
+      type: String,
+      default: 'wrong'
+    },
+    itemActiveClass: {
+      type: String,
+      default: 'active'
+    },
+    itemMargin: {
+      type: String,
+      default: '10px 0'
+    }
+  },
+  data() {
+    return {
+      code: '',
+      itemList: new Array(Number(this.maxLength))
+    }
+  },
+  watch: {
+    code(newVal, oldVal) {
+      if (newVal.length === Number(this.maxLength)) {
+        this.endCallback()
+      }
+
+      if (newVal.length > oldVal.length) {
+        const valArr = newVal.split('')
+        for (let i = 0; i < valArr.length; i++) {
+          this.itemList[i] = valArr[i]
+        }
+      } else if (newVal !== '' || oldVal.length === 1) {
+        this.itemList = new Array(Number(this.maxLength))
+        this.code = ''
+        this.deleteCallback()
+      }
+    }
+  },
+  methods: {
+    deleteCallback() {
+      this.$emit('deleteEvent', this.code)
+    },
+    triggerInput() {
+      this.$refs.codeInput.focus()
+    },
+    endCallback() {
+      this.$emit('finishEvent', this.code)
+    }
+  }
+}
+</script>
+<style scoped>
+.verification-code {
+  text-align: center;
+  position: relative;
+}
+.verification-code__box {
+  margin: 0 auto;
+  position: relative;
+  background: #ffffff;
+}
+
+.verification-code__input {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 0;
+  opacity: 0;
+  outline: none;
+  border: 0;
+}
+
+.verification-code__item {
+  text-align: center;
+  vertical-align: middle;
+}
+.verification-code__item-class {
+  height: 50px;
+  width: 40px;
+  border: 1px solid #f8f8f8;
+  background: #f8f8f8;
+  border-radius: 5px;
+  font-size: 22px;
+  color: #333333;
+}
+.verification-code__item.active {
+  border: 1px solid #14B9C8;
+}
+.verification-code__item.wrong {
+  border: 1px solid #F25A5A;
+}
+</style>
